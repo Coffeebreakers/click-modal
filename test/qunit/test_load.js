b4cc58a98b91c882dc0011b1fbd43029b11be7a7
Rafael Lazarini <jlazarini@sp.r7.com>
2011-02-08 19:30:02 -0200

    Função de teste de load do plugin passed

--- test/qunit/test_load.js
+++ test/qunit/test_load.js
@@ -1,3 +1,7 @@
-test('jQuery.fn.modal()', function () {
-  ok(jQuery.fn.modal(), 'Function working');
+test('jQuery.fn.modal - Call Function', function () {
+  ok(jQuery.fn.modal, 'Function working');
+});
+
+test('jQuery.fn.modal - Call Function openModal', function () {
+  //ok(jQuery.fn.modal.openModal(), 'Function openModal returning');
 });
