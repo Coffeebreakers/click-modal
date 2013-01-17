0f6610c4d6e8aeb50c9dfde2df4d6440f7081d11
Clauber Stipkovic <clauber.halic@gmail.com>
2013-01-17 18:43:43 -0200

    build.js included

---
+++ build/build.js
@@ -0,0 +1,24 @@
+/*jshint node: true, indent: 4*/
+
+"use strict";
+
+var compressor = require('node-minify');
+
+this.compressor.minify({
+    type: 'yui-css',
+    fileIn: 'src/jquery.clickmodal.css',
+    fileOut: 'minified/jquery.clickmodal.min.css',
+    callback: function (err) {
+        console.log(err);
+    }
+});
+
+// Using YUI Compressor for JS
+this.compressor.minify({
+    type: 'yui-js',
+    fileIn: 'src/jquery.clickmodal.js',
+    fileOut: 'minified/jquery.clickmodal.min.js',
+    callback: function (err) {
+        console.log(err);
+    }
+});
