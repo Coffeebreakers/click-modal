c36d4c564bebd0b3e0a667a9db96d885c775ffdb
Clauber Stipkovic <clauber.halic@gmail.com>
2013-09-04 18:00:56 -0300

    test hint removed

--- test/test_jshint.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var jshint = require('jshint').JSHINT, 
-    result = jshint('src/jquery.clickmodal.js');
-
-if (result === false) {
-    console.log(jshint.errors);
-    console.log(jshint.data());
-}
